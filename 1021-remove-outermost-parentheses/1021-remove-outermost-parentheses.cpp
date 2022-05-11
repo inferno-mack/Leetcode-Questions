174aa6afc58931b2d61da1b023ebbb136ddd9fbc
Akash Saini <97120669+inferno-mack@users.noreply.github.com>
2022-05-12 02:47:41 +0530

    Time: 11 ms (15.52%), Space: 8.4 MB (12.09%) - LeetHub

---
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        vector<string> temp;
+        int lo=0,hi=0;
+        int ind=-1;
+        string ans="";
+        for(int i=0;i<s.size();i++)
+        {
+            if(s[i]=='(')
+                lo++;
+            else if(s[i]==')')
+                hi++;
+            
+            if(lo==hi)
+            {
+                temp.push_back(s.substr(ind+1,i-ind));
+                ind=i;
+                lo=0;
+                hi=0;
+            }
+        }
+        
+        
+        for(int i=0;i<temp.size();i++)
+        {
+            cout<<temp[i]<<" ";
+            string str=temp[i];
+            ans+=str.substr(1,str.size()-2);
+        }
+        
+        return ans;
+    }
+};
