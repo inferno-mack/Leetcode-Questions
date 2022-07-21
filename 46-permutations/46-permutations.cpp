fed3ad1e97a0211c5e600b24f52f143a25682724
Akash Saini <97120669+inferno-mack@users.noreply.github.com>
2022-07-22 02:19:50 +0530

    Time: 9 ms (23.30%), Space: 8.1 MB (20.11%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -1,27 +1,20 @@
 class Solution {
 public:
-    void perm(vector<int> &ds, vector<vector<int>> &ans, vector<int> &freq, vector<int> nums){
-        if(ds.size()==nums.size()){
-            ans.push_back(ds);
+    void subsets(int ind, vector<int> nums, vector<vector<int>> &ans){
+        if(ind == nums.size()){
+            ans.push_back(nums);
             return;
         }    
-        
-        for(int i=0;i<nums.size();i++){
-            if(!freq[i]){
-                ds.push_back(nums[i]);
-                freq[i]=1;
-                perm(ds, ans, freq, nums);
-                freq[i]=0;
-                ds.pop_back();
-            }
+        for(int i=ind;i<nums.size();i++){
+            swap(nums[i],nums[ind]);
+            subsets(ind+1, nums, ans);
+            swap(nums[i],nums[ind]);
         }
     }
     
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>> ans;
-        vector<int> ds;
-        vector<int> freq(nums.size(),0);
-        perm(ds, ans, freq, nums);
+        subsets(0, nums, ans);
         return ans;
     }
 };
