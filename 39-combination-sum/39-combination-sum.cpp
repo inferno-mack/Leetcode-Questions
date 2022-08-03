5c70ae60a5801b6bc36fc12e1bbfe8e8b3677d2a
Akash Saini <97120669+inferno-mack@users.noreply.github.com>
2022-08-03 12:56:59 +0530

    Time: 106 ms (24.53%), Space: 69.1 MB (13.74%) - LeetHub

--- 39-combination-sum/39-combination-sum.cpp
+++ 39-combination-sum/39-combination-sum.cpp
@@ -1,27 +1,26 @@
 class Solution {
 public:
     
-    void prints(int ind, vector<int> &ds, vector<int> arr, int target, vector<vector<int>> &ans){
-        if(ind == arr.size()){
-            if(target == 0){
+    void findcombo(int ind, vector<int> candidates, int target, vector<int> &ds, vector<vector<int>> &ans){
+        if(ind == candidates.size()){
+            if(target == 0)
                 ans.push_back(ds);
-            }
             return;
-        } 
+        }
         
-        if(arr[ind]<=target){
-            ds.push_back(arr[ind]);
-            prints(ind, ds, arr, target-arr[ind], ans);
+        if(candidates[ind]<=target){
+            ds.push_back(candidates[ind]);
+            findcombo(ind, candidates, target-candidates[ind], ds, ans);
             ds.pop_back();
         }
         
-        prints(ind+1, ds, arr, target, ans);
+        findcombo(ind+1, candidates, target, ds, ans);
     }
     
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<int> ds;
         vector<vector<int>> ans;
-        prints(0, ds, candidates, target, ans);
+        vector<int> ds;
+        findcombo(0, candidates, target, ds, ans);
         return ans;
     }
 };
