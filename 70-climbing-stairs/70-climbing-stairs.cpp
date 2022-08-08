6ac53c1a3fae742d1f7c8c80d08d621ab8414d8b
Akash Saini <97120669+inferno-mack@users.noreply.github.com>
2022-08-09 01:52:16 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (15.55%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
-    int climb(int ind, vector<int> &dp){
-        if(ind <= 1) return 1;
-        if(dp[ind]!=-1) return dp[ind];
-        return dp[ind]=climb(ind-1, dp)+climb(ind-2, dp);
-    }
     
     int climbStairs(int n) {
         vector<int> dp(n+1,-1);
-        return climb(n, dp);
+        
+        dp[0]=1;
+        dp[1]=1;
+        for(int i=2;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
     }
 };
