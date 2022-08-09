51d5f9c3ef5162b15a2717454cce590645b08d28
Akash Saini <97120669+inferno-mack@users.noreply.github.com>
2022-08-09 14:59:02 +0530

    Time: 0 ms (100.00%), Space: 9.6 MB (6.02%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public:
+    int maxsum(int ind, vector<int> nums, vector<int> &dp){
+        if(ind == 0) return nums[0];
+        if(ind < 0) return 0;
+        
+        if(dp[ind]!=-1) return dp[ind];
+        
+        int pick=nums[ind]+maxsum(ind-2, nums, dp);
+        int notpick=maxsum(ind-1, nums, dp);
+        return dp[ind]=max(pick,notpick);
+    }
+    
     int rob(vector<int>& nums) {
         int n=nums.size();
-        vector<int> dp(n+1,-1);
-        if(n==1) return nums[0];
-        dp[0]=nums[0];
-        
-        for(int i=1;i<n;i++){
-            int take=nums[i];
-            if(i>1) take+=dp[i-2];
-            int nottake=dp[i-1];
-            dp[i]=max(take,nottake);
-        }
-        return dp[n-1];
+        vector<int> dp(n,-1);
+        return maxsum(n-1, nums, dp);
     }
 };
