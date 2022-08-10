87b08706532afb4e71e11ecf87841f56f2cc073f
Akash Saini <97120669+inferno-mack@users.noreply.github.com>
2022-08-10 11:46:00 +0530

    Time: 2 ms (42.60%), Space: 6.3 MB (15.55%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -1,17 +1,15 @@
 class Solution {
 public:
-    int solve(int ind,vector<int> &dp){
-        if(ind==1) return 1;
-        if(ind==2) return 2;
-        
-        if(dp[ind]!=-1) return dp[ind];
-        int fs=solve(ind-1, dp);
-        int ss=solve(ind-2, dp);
-        return dp[ind]=fs+ss;
-    }
-    
     int climbStairs(int n) {
-        vector<int> dp(n+1,-1);
-        return solve(n, dp);
+        if(n==1) return 1;
+        if(n==2) return 2;
+        
+        vector<int> dp(n+1,0);
+        dp[1]=1;
+        dp[2]=2;
+        for(int i=3;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
     }
 };
