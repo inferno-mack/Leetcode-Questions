840c43ff68d72f88ce5a5debaa91f487d13c387a
Akash Saini <97120669+inferno-mack@users.noreply.github.com>
2022-08-17 02:09:25 +0530

    Time: 55 ms (36.97%), Space: 7.4 MB (78.98%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.cpp
+++ 518-coin-change-2/518-coin-change-2.cpp
@@ -2,20 +2,21 @@ class Solution {
 public:
     int change(int val, vector<int>& arr) {
         int n=arr.size();
-        vector<vector<int>> dp(n,vector<int>(val+1,0));
+        vector<int> prev(val+1,0),curr(val+1,0);
         
         for(int tar=0;tar<=val;tar++)
-            dp[0][tar]=(tar%arr[0]==0);
+            prev[tar]=(tar%arr[0]==0);
         
         for(int ind=1;ind<n;ind++){
             for(int tar=0;tar<=val;tar++){
-                int nottake=dp[ind-1][tar];
+                int nottake=prev[tar];
                 int take=0;
-                if(arr[ind]<=tar) take=dp[ind][tar-arr[ind]];
+                if(arr[ind]<=tar) take=curr[tar-arr[ind]];
                 
-                dp[ind][tar]=take+nottake;
+                curr[tar]=take+nottake;
             }
+            prev=curr;
         }
-        return dp[n-1][val];
+        return prev[val];
     }
 };
