e52075a445cc9f0c95ba581474ccfedf38a398c9
Akash Saini <97120669+inferno-mack@users.noreply.github.com>
2022-08-30 19:52:53 +0530

    Time: 1022 ms (5.03%), Space: 216.7 MB (6.52%) - LeetHub

--- 912-sort-an-array/912-sort-an-array.cpp
+++ 912-sort-an-array/912-sort-an-array.cpp
@@ -1,17 +1,47 @@
 class Solution {
 public:
-    vector<int> sortArray(vector<int>& nums) {
-        vector<int> ans;
-        vector<int> freq(100001,0);
-        for(int i=0;i<nums.size();i++){
-            freq[nums[i]+50000]++;
-        }
-        
-        for(int i=0;i<freq.size();i++){
-            while(freq[i]--){
-                ans.push_back(i-50000);
+    void merge(vector<int> &arr, int &low, int &mid, int &high){
+        int i=low, j=mid+1;
+        vector<int> temp;
+        while(i<=mid && j<=high){
+            if(arr[i]<=arr[j]){
+                temp.push_back(arr[i]);
+                i++;
+            }
+            else{
+                temp.push_back(arr[j]);
+                j++;
             }
         }
-        return ans;
+        
+        while(i<=mid){
+            temp.push_back(arr[i]);
+            i++;
+        }
+        
+        while(j<=high){
+            temp.push_back(arr[j]);
+            j++;
+        }
+        
+        int ind=0;
+        for(i=low;i<=high;i++){
+            arr[i]=temp[ind];
+            ind++;
+        }
+    }
+    
+    void mergesort(vector<int> &arr, int low, int high){
+        if(low<high){
+            int mid=low+(high-low)/2;
+            mergesort(arr,low,mid);
+            mergesort(arr,mid+1,high);
+            merge(arr,low,mid,high);
+        }
+    }
+    
+    vector<int> sortArray(vector<int>& nums) {
+        mergesort(nums,0,nums.size()-1);
+        return nums;
     }
 };
