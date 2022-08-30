b45971358836e6fc355bebe116b8de19342e2826
Akash Saini <97120669+inferno-mack@users.noreply.github.com>
2022-08-30 19:38:17 +0530

    Time: 143 ms (38.69%), Space: 54.4 MB (25.49%) - LeetHub

--- 912-sort-an-array/912-sort-an-array.cpp
+++ 912-sort-an-array/912-sort-an-array.cpp
@@ -1,7 +1,17 @@
 class Solution {
 public:
     vector<int> sortArray(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        return nums;
+        vector<int> ans;
+        vector<int> freq(100001,0);
+        for(int i=0;i<nums.size();i++){
+            freq[nums[i]+50000]++;
+        }
+        
+        for(int i=0;i<freq.size();i++){
+            while(freq[i]--){
+                ans.push_back(i-50000);
+            }
+        }
+        return ans;
     }
 };
