bc893caf7f479daf0bd0f16eaff873ec04708024
Akash Saini <97120669+inferno-mack@users.noreply.github.com>
2022-08-31 13:39:36 +0530

    Time: 19 ms (33.44%), Space: 14.2 MB (34.16%) - LeetHub

--- 733-flood-fill/733-flood-fill.cpp
+++ 733-flood-fill/733-flood-fill.cpp
@@ -1,39 +1,23 @@
 class Solution {
 public:
+    void dfs(vector<vector<int>> &grid, vector<vector<int>> &vis, int i, int j, int m, int n, int color){
+        vis[i][j]=1;
+        if((i-1)>=0 and grid[i-1][j]==grid[i][j] and vis[i-1][j]==-1)
+            dfs(grid, vis, i-1, j, m, n, color);
+        if((i+1)<m and grid[i+1][j]==grid[i][j] and vis[i+1][j]==-1)
+            dfs(grid, vis, i+1, j, m, n, color);
+        if((j-1)>=0 and grid[i][j-1]==grid[i][j] and vis[i][j-1]==-1)
+            dfs(grid, vis, i, j-1, m, n, color);
+        if((j+1)<n and grid[i][j+1]==grid[i][j] and vis[i][j+1]==-1)
+            dfs(grid, vis, i, j+1, m, n, color);
+        grid[i][j]=color;
+    }
+    
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
         int m=image.size();
         int n=image[0].size();
-        queue<pair<int,int>> q;
-        q.push({sr,sc});
         vector<vector<int>> vis(m,vector<int>(n,-1));
-        while(!q.empty()){
-            pair<int,int> p=q.front();
-            int i=p.first, j=p.second;
-            q.pop();
-            
-            vis[i][j]=1;
-            if((i-1)>=0 and image[i-1][j]==image[i][j] and vis[i-1][j]==-1){
-                // image[i-1][j]=color;
-                q.push({i-1,j});
-                vis[i-1][j]=1;
-            }
-            if((i+1)<m and image[i+1][j]==image[i][j] and vis[i+1][j]==-1){
-                // image[i+1][j]=color;
-                q.push({i+1,j});
-                vis[i+1][j]=1;
-            }
-            if((j-1)>=0 and image[i][j-1]==image[i][j] and vis[i][j-1]==-1){
-                // image[i][j-1]=color;
-                q.push({i,j-1});
-                vis[i][j-1]=1;
-            }
-            if((j+1)<n and image[i][j+1]==image[i][j] and vis[i][j+1]==-1){
-                // image[i][j+1]=color;
-                q.push({i,j+1});
-                vis[i][j+1]=1;
-            }
-            image[i][j]=color;
-        }
+        dfs(image, vis, sr, sc, m, n, color);
         return image;
     }
 };
