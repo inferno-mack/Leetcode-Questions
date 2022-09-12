9545adaa83ba92f2610d20d63abca7c006bf85b4
Akash Saini <97120669+inferno-mack@users.noreply.github.com>
2022-09-12 23:49:00 +0530

    Time: 603 ms (19.77%), Space: 114.1 MB (12.36%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -1,27 +1,24 @@
 class Solution {
 public:
+    bool f(int ind, int target, vector<int> &nums, vector<vector<int>> &dp){
+        if(target == 0) return true;
+        if(ind == 0) return (nums[0]==target);
+        
+        if(dp[ind][target]!=-1) return dp[ind][target];
+        bool nottake=f(ind-1, target, nums,dp);
+        bool take=false;
+        if(nums[ind]<=target) take=f(ind-1, target-nums[ind], nums,dp);
+        return dp[ind][target]=take | nottake;
+    }
+    
     bool canPartition(vector<int>& nums) {
         int n=nums.size();
         int sum=0;
-        for(auto it:nums){
+        for(auto it:nums)
             sum+=it;
-        }
-        if(sum & 1) return false;
-        int k=sum/2;
-        vector<bool> prev(k+1,0), curr(k+1,0);
-        prev[0]=curr[0]=true;
-        if(nums[0]<=k) prev[nums[0]]=true;
         
-        for(int ind=1;ind<n;ind++){
-            for(int target=1;target<=k;target++){
-                bool nottake=prev[target];
-                bool take=false;
-                if(nums[ind]<=target) 
-                    take=prev[target-nums[ind]];
-                curr[target]=take|nottake;
-            }
-            prev=curr;
-        }
-        return prev[k];
+        vector<vector<int>> dp(n, vector<int>(sum/2+1, -1));
+        if(sum & 1) return false;
+        return f(n-1, sum/2, nums, dp);
     }
 };
