106aa7e3208e28af30cc50fa70ce804a1d92ce6a
Akash Saini <97120669+inferno-mack@users.noreply.github.com>
2022-09-13 00:03:24 +0530

    Time: 745 ms (11.76%), Space: 14.2 MB (54.68%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -17,8 +17,21 @@ public:
         for(auto it:nums)
             sum+=it;
         
-        vector<vector<int>> dp(n, vector<int>(sum/2+1, -1));
+        int k=sum/2;
+        vector<vector<bool>> dp(n+1, vector<bool>(k+1, false));
         if(sum & 1) return false;
-        return f(n-1, sum/2, nums, dp);
+        for(int i=0;i<n;i++)
+            dp[i][0]=true;
+        
+        if(nums[0]<=k) dp[0][nums[0]]=true;
+        for(int ind=1;ind<n;ind++){
+            for(int target=1;target<=k;target++){
+                bool nottake=dp[ind-1][target];
+                bool take=false;
+                if(nums[ind]<=target) take=dp[ind-1][target-nums[ind]];
+                dp[ind][target]=take | nottake;
+            }
+        }
+        return dp[n-1][k];
     }
 };
