edd897066cbb341fcf19ae0ab18cd5cf3b6eaf52
Akash Saini <97120669+inferno-mack@users.noreply.github.com>
2022-09-12 17:02:01 +0530

    Time: 31 ms (33.03%), Space: 20.6 MB (25.56%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.cpp
+++ 42-trapping-rain-water/42-trapping-rain-water.cpp
@@ -1,24 +1,20 @@
 class Solution {
 public:
-    int trap(vector<int>& heights) {
+    int trap(vector<int>& height) {
         int res=0;
-        int n=heights.size();
-        vector<int> mxl(n);
-        vector<int> mxr(n);
-        mxl[0]=heights[0];
-        mxr[n-1]=heights[n-1];
+        int n=height.size();
         
-        for(int i=1;i<n;i++){
-            mxl[i]=max(mxl[i-1],heights[i]);
-        }
+        vector<int> prefixsum(n, height[0]);
+        vector<int> suffixsum(n, height[n-1]);
         
-        for(int j=n-2;j>=0;j--){
-            mxr[j]=max(mxr[j+1],heights[j]);
-        }
-
-        for(int i=0;i<n;i++){
-            res=res+(min(mxr[i],mxl[i])-heights[i]);
-        }
+        for(int i=1;i<height.size();i++)
+            prefixsum[i]=max(prefixsum[i-1],height[i]);
+        
+        for(int i=height.size()-2;i>=0;i--)
+            suffixsum[i]=max(suffixsum[i+1],height[i]);
+        
+        for(int i=0;i<height.size();i++)
+            res+=min(prefixsum[i], suffixsum[i]) - height[i];
         
         return res;
     }
