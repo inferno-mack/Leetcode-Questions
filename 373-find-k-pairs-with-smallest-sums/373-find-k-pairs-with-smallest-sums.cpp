36b2a2079e3a7010ec8d081973ff16839c963acc
Akash Saini <97120669+inferno-mack@users.noreply.github.com>
2022-09-20 23:44:06 +0530

    Time: 731 ms (19.87%), Space: 110 MB (74.46%) - LeetHub

--- 373-find-k-pairs-with-smallest-sums/373-find-k-pairs-with-smallest-sums.cpp
+++ 373-find-k-pairs-with-smallest-sums/373-find-k-pairs-with-smallest-sums.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
-        priority_queue<pair<int,pair<int,int>>> pq;
+        priority_queue<pair<int, pair<int,int>>> pq;
         
         for(int i=0;i<nums1.size();i++){
             for(int j=0;j<nums2.size();j++){
@@ -24,7 +24,7 @@ public:
             ans.push_back({pq.top().second.first, pq.top().second.second});
             pq.pop();
         }
-        reverse(ans.begin(),ans.end());
+        reverse(ans.begin(), ans.end());
         return ans;
     }
 };
