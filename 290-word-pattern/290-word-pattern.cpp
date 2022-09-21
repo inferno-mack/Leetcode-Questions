aa92a04e68c299d9b005f739ae1387017e329571
Akash Saini <97120669+inferno-mack@users.noreply.github.com>
2022-09-22 00:56:16 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (96.02%) - LeetHub

---
+++ 290-word-pattern/290-word-pattern.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool wordPattern(string pattern, string s) {
+        vector<string> vec;
+        string temp="";
+        for(int i=0;i<s.size();i++){
+            while(s[i]!=' ' and i<s.size()){
+                temp+=s[i];
+                i++;
+            }
+            vec.push_back(temp);
+            temp="";
+        }
+        vec.push_back(temp);
+        
+        if((vec.size()-1)!=pattern.size())
+            return false;
+        
+        unordered_map<char, string> mp;
+        unordered_map<string, char> mp1;
+        for(int i=0;i<pattern.size();i++){
+            if(mp.count(pattern[i]) && mp1.count(vec[i])){
+                if(mp[pattern[i]]!=vec[i] || mp1[vec[i]]!=pattern[i])
+                    return false;
+            }
+            else if(mp.count(pattern[i])==false && mp1.count(vec[i])==false){
+                mp[pattern[i]]=vec[i];
+                mp1[vec[i]]=pattern[i];
+            }
+            else
+                return false;
+        }
+        return true;
+    }
+};
