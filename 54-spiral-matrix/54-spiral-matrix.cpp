bcfd5d12d2fdadf98c278c1ba0a658ed5901dbef
Akash Saini <97120669+inferno-mack@users.noreply.github.com>
2022-09-22 01:40:07 +0530

    Time: 0 ms (100.00%), Space: 6.8 MB (91.75%) - LeetHub

--- 54-spiral-matrix/54-spiral-matrix.cpp
+++ 54-spiral-matrix/54-spiral-matrix.cpp
@@ -9,34 +9,30 @@ public:
         
         while(top<=bottom and left<=right){
             if(direction==1){
-                for(int i=left;i<=right;i++){
+                for(int i=left;i<=right;i++)
                     ans.push_back(matrix[top][i]);
-                }
                 top++;
                 direction=2;
             }
             else if(direction==2){
-                for(int i=top;i<=bottom;i++){
+                for(int i=top;i<=bottom;i++)
                     ans.push_back(matrix[i][right]);
-                }
                 right--;
                 direction=3;
             }
             else if(direction==3){
-                for(int i=right;i>=left;i--){
+                for(int i=right;i>=left;i--)
                     ans.push_back(matrix[bottom][i]);
-                }
                 bottom--;
                 direction=4;
             }
             else if(direction==4){
-                for(int i=bottom;i>=top;i--){
+                for(int i=bottom;i>=top;i--)
                     ans.push_back(matrix[i][left]);
-                }
                 left++;
                 direction=1;
+            }
         }
-    }
         return ans;
     }
 };
