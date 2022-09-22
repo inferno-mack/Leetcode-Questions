1b62fa27fd6ebc794b35bbf90c3b1d9c4d88a052
Akash Saini <97120669+inferno-mack@users.noreply.github.com>
2022-09-22 13:28:51 +0530

    Time: 10 ms (93.68%), Space: 21.5 MB (89.10%) - LeetHub

---
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
@@ -0,0 +1,66 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    /* Returns the maximum value in root node's subtree */
+int bstMax(TreeNode* root)
+{
+int mx = root -> val;
+if(root -> left)
+mx = max(mx , bstMax(root -> left)) ;
+if(root -> right)
+mx = max(mx , bstMax(root -> right)) ;
+return mx;
+}
+/* Returns the minimum value in root node's subtree */
+int bstMin(TreeNode* root)
+{
+	int mn = root -> val;
+	if (root -> left)
+		mn = min(mn , bstMin(root -> left)) ;
+	if (root -> right)
+		mn = min(mn , bstMin(root -> right));
+	return mn;
+}
+bool isValidBST (TreeNode* root)
+{
+	if (root == NULL || (root -> right == NULL && root -> left == NULL)) return true ;
+	if (root -> left && root -> right)
+	{
+		int leftMax = bstMax (root->left);
+		int rightMin = bstMin(root->right);
+// Return false if root node's value does not follow BST Property
+		if (leftMax > root->val || rightMin < root->val)
+			return false;
+	}
+	if (root -> left)
+	{
+		int leftMax = bstMax (root->left);
+		if (leftMax >= root->val)
+			return false;
+	}
+	if (root -> right)
+	{
+		int rightMin = bstMin(root->right);
+		if (rightMin <= root->val)
+			return false;
+	}
+// Recursively check if left and right subtrees are BST or not
+	bool left = isValidBST (root->left);
+	bool right = isValidBST (root->right) ;
+	if (left == true && right == true)
+		return true;
+	else
+		return false;
+}
+
+};
