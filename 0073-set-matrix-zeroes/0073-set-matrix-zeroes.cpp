bc1291c1a3f33be4d935995c4a1c0da1218cc4c6
Akash Saini <97120669+inferno-mack@users.noreply.github.com>
2022-11-08 12:52:09 +0530

    Time: 33 ms (42.17%), Space: 13.2 MB (55.67%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -4,20 +4,28 @@ public:
         int m=matrix.size();
         int n=matrix[0].size();
         
-        vector<vector<int>> vis = matrix;
-        
+        vector<int> row(m,-1), col(n,-1);
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(matrix[i][j]==0){
-                    for(int k=0;k<n;k++)
-                        vis[i][k]=0;
-                    
-                    for(int l=0;l<m;l++)
-                        vis[l][j]=0;
+                    row[i]=0;
+                    col[j]=0;
                 }
             }
         }
         
-        matrix = vis;
+        for(int r=0;r<m;r++){
+            if(row[r]==0){
+                for(int j=0;j<n;j++)
+                    matrix[r][j] = 0;
+            }
+        }
+        
+        for(int c = 0; c < n; c++){
+            if(col[c] == 0){
+                for(int i = 0; i < m ; i++)
+                    matrix[i][c] = 0;
+            }
+        }
     }
 };
